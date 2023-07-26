9946c9ea3d893c3f6ebe40d2ba8ffc2a104e0444
pythondev1998 <pythondevelopcode@gmail.com>
2023-07-26 15:53:07 -0400

    FIX

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # ---------------------------- CONNECT TO DATABASE ------------------------------- #
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///cafes-banff.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
